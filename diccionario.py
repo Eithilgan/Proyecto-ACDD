9b02c21b0bba064c6b62b3defbdd3bca108dadc1
Dennis R <dennis.riffo2015@alu.uct.cl>
2019-09-04 11:00:01 -0400

    diccionario y plabras claves

---
+++ diccionario.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 04 10:38:26 2019
+
+@author: Ezxiio
+"""
+
+diccionario = dict()
+diccionario = {}
+diccionario["Agricultura"] = ["hola","chao","adios"]
+
+
+print diccionario
