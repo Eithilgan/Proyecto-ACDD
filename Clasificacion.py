ee9d91349f6a6e0c4a49611b522027e867eb2a1d
Dennis R <dennis.riffo2015@alu.uct.cl>
2019-09-24 18:17:00 -0300

    clasificacion update

--- Clasificacion.py
+++ Clasificacion.py
@@ -9,40 +9,42 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.neighbors import KNeighborsClassifier
 
-prepositions =['a','ante','bajo','cabe','con','contra','de','desde','en','entre','hacia','hasta','para','por','según','sin','so','sobre','tras']
-prep_alike = ['durante','mediante','excepto','salvo','incluso','más','menos']
-adverbs = ['no','si','sí']
-articles = ['el','la','los','las','un','una','unos','unas','este','esta','estos','estas','aquel','aquella','aquellos','aquellas']
-aux_verbs = ['he','has','ha','hemos','habéis','han','había','habías','habíamos','habíais','habían']
-pronouns = ['yo', 'tú', 'él', 'nosotros', 'ustedes', 'ellos', 'que', 'quien', 'me', 'mi', 'su','niños','niñas','adolecentes','niño','niña','diputado','diputada','diputados','diputadas','intervencion','interviene']
+preposiciones =['a','ante','bajo','cabe','con','contra','de','desde','en','entre','hacia','hasta','para','por','según','sin','so','sobre','tras']
+prep_aux = ['durante','mediante','excepto','salvo','incluso','más','menos']
+adverbios = ['no','si','sí']
+personales = ['el','la','los','las','un','una','unos','unas','este','esta','estos','estas','aquel','aquella','aquellos','aquellas']
+verbos_aux = ['he','has','ha','hemos','habéis','han','había','habías','habíamos','habíais','habían']
+pronombres = ['yo', 'tú', 'él', 'nosotros', 'ustedes', 'ellos', 'que', 'quien', 'me', 'mi', 'su','niños','niñas','adolecentes','niño','niña','diputado','diputada','diputados','diputadas','intervencion','interviene']
 
 #TfidfVectorizer crea una matriz de tipo frecuencia de término – frecuencia inversa de documento 
 #(o sea, la frecuencia de ocurrencia del término en la colección de documentos)
 #como en este caso le estamos indicando "stop_words", esto quiere decir que 
 #las palabras pertenecientes a la matriz TF-IDF si bien se repetiran 
 # una gran cantidad de veces, estas no seran de mucha importancia.
-tfid = TfidfVectorizer(stop_words=prepositions+prep_alike+adverbs+articles+aux_verbs+pronouns)
+tfid = TfidfVectorizer(stop_words=preposiciones+prep_aux+adverbios+personales+verbos_aux+pronombres)
 
 
-def read_all_documents(root):
-    labels = []
-    docs = []
+def leer_documentos(root):
+    labels = [] #tema
+    docs = []   #
     for r, dirs, files in os.walk(root):
         for file in files:
             with open(os.path.join(r, file), "r",encoding="latin-1") as f:
-                docs.append(f.read())
-            labels.append(r.replace(root, ''))
+                #r es el directorio, y file son los archivos txt dentro, lo que hace
+                #esta linea es unir la ruta de forma inteligente
+                docs.append(f.read()) #todo el contenido de cada txt
+            labels.append(r.replace(root, '')) #guarda nombre de cada carpeta y quita el nombre del path principal
     return dict([('docs', docs), ('labels', labels)])
-
+    #devuelve diccionario con el texto y los nombres de cada carpeta 
 #____________________________________________________________#
 #data se transforma en un diccionario con los temas y texto correspondiente para cada tema
-data = read_all_documents('Palabras_claves')
+data = leer_documentos('Palabras_claves')
 #se separan el texto y los temas: Documents = texto Labels = temas
-documents = data['docs']
-labels = data['labels']
+documentos = data['docs']
+etiquetas = data['labels']
 #____________________________________________________________#
-X_train = tfid.fit_transform(documents)
-y_train = labels
+X_train = tfid.fit_transform(documentos)
+y_train = etiquetas
 #____________________________________________________________#
 #KNeighborsClassifier(n_neighbors=3) es un metodo de clasificacion que asigna
 #una clase dependiendo de la cantidad de representantes mas cercanos que este posea
@@ -51,7 +53,7 @@ clf = KNeighborsClassifier(n_neighbors=3)
 clf.fit(X_train, y_train)
 
 #____________________________________________________________#
-test = read_all_documents('Documento')
+test = leer_documentos('Documento')
 X_test = tfid.transform(test['docs'])
 y_test = test['labels']
 pred = clf.predict(X_test)
