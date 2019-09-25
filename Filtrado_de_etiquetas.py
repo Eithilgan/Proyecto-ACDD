29cb9626daafce46ef458308b7fdf05cb8badf69
Francisco Gutierrez <30905688+FranciscoGutierrez18@users.noreply.github.com>
2019-09-25 10:25:34 -0300

    Actualizacion diagrama y variables

--- Filtrado_de_etiquetas.py
+++ Filtrado_de_etiquetas.py
@@ -13,29 +13,29 @@ soup = BeautifulSoup(contenido, "lxml")
 
 #encontramos TODAS las etiquetas y las guardamos en arreglos "alltags"
 #alltags contiene el nombre de las etiquetas y alltagscontent su contenido
-alltags = [tag.name for tag in soup.find_all()]
-alltagscontent = [tag.text for tag in soup.find_all()]
+allTagsID = [tag.name for tag in soup.find_all()]
+allTagsContent = [tag.text for tag in soup.find_all()]
 
 #imprimir todas las etiquetas excepto "br", la cual se usa solo para hacer salto de linea
 def lista_etiquetas():
-    for i in range(len(alltags)):
-        if alltags[i]!="br":   
-            print (alltags[i])
+    for i in range(len(allTagsID)):
+        if allTagsID[i]!="br":   
+            print (allTagsID[i])
 
 #funcion para encontrar una etiqueta en específico
 def buscar_etiqueta(etiqueta):
-    for i in range(len(alltagscontent)):
-        if alltags[i]==etiqueta:  
-            return (alltagscontent[i])
+    for i in range(len(allTagsContent)):
+        if allTagsID[i]==etiqueta:  
+            return (allTagsContent[i])
             break
 
 #imprime TODO el contenido de TODAS LAS INSTANCIAS de una etiqueta
 #enumeradas y ordenadas
 def buscar_contenido_etiquetas(etiqueta):
     cont = 1
-    for i in range(len(alltagscontent)):
-        if alltags[i]==etiqueta:  
-            print (etiqueta + " NUMERO: " + str(cont) + "\n" + (alltagscontent[i]))
+    for i in range(len(allTagsContent)):
+        if allTagsID[i]==etiqueta:  
+            print (etiqueta + " NUMERO: " + str(cont) + "\n" + (allTagsContent[i]))
             cont = cont+1
 
 
