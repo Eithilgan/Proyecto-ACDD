a916bbbdd2b86bc823880beaf834af7f94d9c627
Eithilgan <dennis.riffo2015@alu.uct.cl>
2019-10-08 23:38:43 -0300

    analisis

--- Filtrado_de_etiquetas.py
+++ Filtrado_de_etiquetas.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 
 
 #scraping de boletin, guardando el contenido en variable "contenido"
-fuente = requests.get("http://opendata.congreso.cl/wscamaradiputados.asmx/getSesionBoletinXML?prmSesionID=32")
+fuente = requests.get("http://opendata.congreso.cl/wscamaradiputados.asmx/getSesionBoletinXML?prmSesionID=3747")
 contenido = fuente.content
 #print (contenido)
 
@@ -18,16 +18,18 @@ allTagsContent = [tag.text for tag in soup.find_all()]
 
 #imprimir todas las etiquetas excepto "br", la cual se usa solo para hacer salto de linea
 def lista_etiquetas():
+    etiquetas = []
     for i in range(len(allTagsID)):
         if allTagsID[i]!="br":   
             print (allTagsID[i])
+            etiquetas.append(allTagsID[i])
 
 #funcion para encontrar una etiqueta en específico
 def buscar_etiqueta(etiqueta):
     for i in range(len(allTagsContent)):
         if allTagsID[i]==etiqueta:  
             return (allTagsContent[i])
-            break
+        
 
 #imprime TODO el contenido de TODAS LAS INSTANCIAS de una etiqueta
 #enumeradas y ordenadas
@@ -37,6 +39,13 @@ def buscar_contenido_etiquetas(etiqueta):
         if allTagsID[i]==etiqueta:  
             print (etiqueta + " NUMERO: " + str(cont) + "\n" + (allTagsContent[i]))
             cont = cont+1
+
+#-----------------------------------------------------------------------------------------
+eti_pref = ['titulo','asistencia','intervencion_diputado']
+lista_etiquetas()
+for x in eti_pref:
+    print(buscar_etiqueta(x))
+    
 
 
 
